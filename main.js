8f798e42e0904dcffd2a349e7319fea428501301
Simone Mandorlini <djbaffo85@gmail.com>
2025-06-11 23:23:22 +0200

    Modificati index e main

---
+++ main.js
@@ -0,0 +1,22 @@
+let num1 = 14;
+let num2 = 3; 
+
+console.log(num1);
+console.log(num2);
+
+let stringa = "Questa è una stringa";
+
+console.log(stringa);
+
+stringa = "Stessa variabile, contenuto diverso";
+
+console.log(stringa);
+
+
+const PIGRECO = 3.14159265359;
+
+console.log(PIGRECO);
+
+PIGRECO = 3.14;
+
+
